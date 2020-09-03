87a5b837bab7527f7907319a6731055b0c7543df
indraNarbutaite <indranarbutaite520@gmail.com>
2020-09-03 18:55:26 +0300

    Adding initial project files

---
+++ src/com/indra/app/HelloWord.java
@@ -0,0 +1,10 @@
+package com.indra.app;
+
+public class HelloWord {
+    public static class HelloWorld {
+        public static void main(String[] args) {
+            System.out.println("Hello, World!");
+
+        }
+    }
+}
